019a62694502d42591be6f0b161ec10f91c90f3c
Robson Giordani <robsoncgiordani@yahoo.com.br>
2026-01-28 17:47:25 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,5 @@
-Fernanda eu te amo. Se você me ama, dá uma risadinha 😆
+import { hookPropertyMap } from "next/dist/server/require-hook";
+
+console.log("Fernanda eu te amo. Se você me ama, dá uma risadinha 😆");
+
+export default home;
